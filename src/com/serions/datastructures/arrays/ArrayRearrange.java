91d5952c3868f9088f390726c5907dbecfe47a02
John Seremba <john.seremba@sumup.com>
2021-08-02 19:38:10 +0200

    Reverse an array
    Algorithm to reverse an array
     * https://www.geeksforgeeks.org/write-a-program-to-reverse-an-array-or-string/

--- src/com/serions/datastructures/arrays/ArrayRearrange.java
+++ src/com/serions/datastructures/arrays/ArrayRearrange.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 public class ArrayRearrange {
     public static void main(String[] args) {
+        reverseArray(new int[]{1,2,3});
         rearrange(new int[]{-1, -1, 6, 1, 9, 3, 2, -1, 4, -1});
     }
 
@@ -20,6 +21,23 @@ public class ArrayRearrange {
                 arr[i] = temp;
             }
         }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    /**
+     * Algorithm to reverse an array
+     * https://www.geeksforgeeks.org/write-a-program-to-reverse-an-array-or-string/
+     * @param arr
+     */
+    private static void reverseArray(int[] arr) {
+        int j = arr.length - 1;
+        for (int i = 0; i < j; i++) {
+            int temp = arr[j];
+            arr[j] = arr[i];
+            arr[i] = temp;
+            j--;
+        }
+
         System.out.println(Arrays.toString(arr));
     }
 }
