227210077166e0e85160218592ccc7cac42a672e
John Seremba <john.seremba@sumup.com>
2021-08-03 17:00:54 +0200

    Check if a given string is a valid number of floating piont number
    In Set 1, we have discussed general approach to check whether a string is a valid number or not. In this post, we will discuss regular expression approach to check for a number.
    
    Examples:
    
    Input : str = "11.5"
    Output : true
    
    Input : str = "abc"
    Output : false
    
    Input : str = "2e10"
    Output : true
    
    Input : 10e5.4
    Output : false

--- src/com/serions/datastructures/strings/StringLiterals.java
+++ src/com/serions/datastructures/strings/StringLiterals.java
@@ -1,9 +1,20 @@
 package com.serions.datastructures.strings;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringLiterals {
     public static void main(String[] args) {
         String result = swap("geeks for geeks");
         System.out.println(result);
+        boolean result2 =  match("abc");
+        System.out.println(result2);
+        boolean result3 = match("1234");
+        System.out.println(result3);
+        boolean result4 = match("123.4");
+        System.out.println(result4);
+        boolean result5 = match("123e4");
+        System.out.println(result5);
     }
 
     // Naive approach
@@ -20,13 +31,37 @@ public class StringLiterals {
 
     private static String swap(String str) {
         char[] chars = str.toCharArray();
-        for(int i = 0; i < chars.length; i++) {
+        for (int i = 0; i < chars.length; i++) {
             int k = i;
-            while(i < chars.length && chars[i] != ' ') i++;
+            while (i < chars.length && chars[i] != ' ') i++;
             char temp = chars[k];
             chars[k] = chars[i - 1];
             chars[i - 1] = temp;
         }
         return new String(chars);
+    }
+
+    /**
+     * Check if a given string is a valid number
+     * input str = "11.5" | Output: true
+     * input str = "abc" | Output: false
+     * input str = "2e10" | Output: true
+     */
+    private static boolean match(String str) {
+        // Regular expression for a floating poing integer number
+        String regex = "[-+]?[0-9]+(\\.[0-9]+)?([Ee][+-]?[0-9]+)?";
+
+        // Compiling regex
+        Pattern p = Pattern.compile(regex);
+
+        // Matcher that will match input against regex
+        Matcher m = p.matcher(str);
+
+        // If match found and equal to input
+        if (m.find() && m.group().equals(str)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
